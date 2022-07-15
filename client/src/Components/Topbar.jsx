a467216a561b00fadc21163f8bab91aef4cf82ac
Siam Ahmed <ahmedsiam397793@gmail.com>
2022-07-15 23:56:38 +0600

    topbar fixes

--- client/src/Components/Topbar.jsx
+++ client/src/Components/Topbar.jsx
@@ -17,26 +17,19 @@ const Topbar = () => {
                 className="flex items-center justify-between h-16 mx-auto max-w-screen-2xl sm:px-6 lg:px-8"
             >
                 <div className="flex items-center">
-                    <button type="button" className="p-2 sm:mr-4 lg:hidden">
+                    <button type="button" className="p-2">
                         <svg
-                            className="w-6 h-6"
+                            className="w-8 h-8"
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
                         >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M4 6h16M4 12h16M4 18h16"
-                            />
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <path d="M4 21v-13a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-9l-4 4" />
+                            <line x1="8" y1="9" x2="16" y2="9" />  <line x1="8" y1="13" x2="14" y2="13" />
                         </svg>
                     </button>
-
-                    <a href="" className="flex">
-                        <span className="inline-block w-32 h-10 bg-gray-200 rounded-lg"></span>
-                    </a>
                 </div>
 
                 <div className="flex items-center justify-end flex-1">
@@ -77,8 +70,8 @@ const Topbar = () => {
                         >
 
                             <span>
-                                <a
-                                    href="/account"
+                                <NavLink
+                                    to="/account"
                                     className="block p-6 border-b-4 border-transparent hover:border-red-700"
                                 >
                                     <svg
@@ -97,7 +90,7 @@ const Topbar = () => {
                                     </svg>
 
                                     <span className="sr-only"> Account </span>
-                                </a>
+                                </NavLink>
                             </span>
 
                             <span className="hidden sm:block">
